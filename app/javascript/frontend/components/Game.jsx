526373cb6332b6d7b386a3c0f466201145b2baea
Nikolaos Emmanouilidis <121699424+NikEmman@users.noreply.github.com>
2024-11-16 22:42:01 +0200

    Get static images to react

--- app/javascript/frontend/components/Game.jsx
+++ app/javascript/frontend/components/Game.jsx
@@ -5,10 +5,12 @@ export default function Game() {
     <div className="game">
       <main>
         <div className="gameNav">
-          This is where char icons get displayed
+          <img src="/images/waldo_sprite.jpg" alt="waldo sprite" />
           <div className="timer">10:12:07</div>
         </div>
-        <div className="image">This is where image is rendered</div>
+        <div className="gameImage">
+          <img src="/images/easy.jpg" alt="Easy Image" />
+        </div>
       </main>
       <section>
         <h3>This is section content</h3>
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  root "homepage#index"
- get "*path", to: "homepage#index", constraints: ->(req) { req.format.html? }
- end
+  root to: "homepage#index"
+
+  # Specific routes for your app
+  get "/leaderboard", to: "homepage#index"
+  get "/game", to: "homepage#index"
+
+  # Catch-all route for React Router with exclusion for static files
+  get "*path", to: "homepage#index", constraints: ->(req) { req.format.html? && !req.path.start_with?("/images", "/assets") }
+end
